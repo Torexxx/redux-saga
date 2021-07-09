20807f30cf87b8972d5722adc01e942568fb3d64
torexxx <molento-studio@yandex.ru>
2021-07-09 13:09:41 +0300

    redux-saga effects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
 
-  const store = useSelector(store => store);
-  console.log(store)
+  const store = useSelector(state => state);
+  const people = useSelector(state => state.people);
+  const dispatch = useDispatch();
 
   return (
     <div>
       Redux saga
+      <button onClick = {() => dispatch({type: 'LOAD_DATA', payload: people}) }>Click</button>
     </div>
   );
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,3 +1,38 @@
+import {take, takeEvery, takeLatest, takeLeading, put, call, fork} from 'redux-saga/effects';
+
+// const wait = (t) => new Promise(resolve => {
+//     setTimeout(resolve, t);
+// })
+
+async function swapiGet (pattern) {
+    const request =  await fetch(`http://swapi.dev/api/${pattern}/`);
+    const data = await request.json();
+
+    return data;
+}
+
+function* loadPeople() {
+    const people = yield call(swapiGet, 'people');
+    
+    yield put({type: 'SET_PEOPLE', payload: people.results});
+}
+function* loadPlanets() {
+    const planets = yield call(swapiGet, 'planets');
+
+    yield put({type: 'SET_PLANETS', payload: planets.results});
+}
+
+export function* workerSaga () {
+    yield fork(loadPeople);
+    yield fork(loadPlanets);
+ 
+}
+
+export function* watchLoadDataSaga () {
+    yield takeEvery('LOAD_DATA', workerSaga);
+    
+}
+
 export default function* rootSaga() {
-    console.log('rootSaga');
+    yield fork(watchLoadDataSaga);
 }
